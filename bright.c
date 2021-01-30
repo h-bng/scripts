6a87e28b596f9e24d0fe6e6a2b668c0114456a21
H. Bingol <h.bingol@pm.me>
2021-01-30 13:59:36 +0300

    usage added

--- bright.c
+++ bright.c
@@ -1,7 +1,20 @@
+/* 
+ * bright.c: an inefficient way to change screen brightness
+ *
+ * change B_PATH for your own system.
+ * set MAX_B and MIN_B to desired values.
+ *
+ * usage:
+ * bright      get current brightness
+ * bright  B   set brightness to B%
+ * bright +b   increase brightness by b%
+ * bright -b   decrease brightness by b%
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-/* CHANGE THESE FOR YOUR OWN SYSTEM */
 #define B_PATH "/sys/class/backlight/intel_backlight/brightness"
 #define MIN_B 9
 #define MAX_B 937
